75d5825171dac3a378028e3aa033b02672aeae49
Tewodros Desta <teddydesta2021@gmail.com>
2023-10-23 12:31:27 -0700

    update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,22 +5,23 @@ const LandingPage = () => {
   return (
 
     
-<div className='mx-auto  m-5 mb-5  w-1/3  pb-20 
-    flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500
+<div className='mx-auto  m-5 mb-5  w-1/3  pb-40  pt-20
+    flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500  
+    text-gradient-to-r from-sky-500 to-indigo-500 
      shadow-md boreder-2 border-white-900 
   rounded-lg  text-white  ' >
         <div className='justify-center  items-center p-10 '>
-            <h1 className='text-4xl font-bold justify-content text-center mb-10'>
+            <h1 className='text-4xl font-bold justify-content text-center mb-10 text-yellow-500'>
               Track and Analyze Your Sleep Pattern
               </h1>
-            <p className='justify-content text-2xl font-semibold text-center'>Track sleep efficiency by measuring your movement through the night</p>
+            <p className='justify-content text-3xl font-semibold text-center'>Track sleep efficiency by measuring your movement through the night</p>
 
         </div>
-        <div className='justify-center  items-center mx-auto '>
+        {/* <div className='justify-center  items-center mx-auto '>
             <img
             className='justify-center items-center border-none rounded-md object-fit'
             src="src/assets/stopwatch.png" alt="" />
-        </div>
+        </div> */}
 
         <div className='h-12 w-1/2 hover:from-pink-500 hover:to-yellow-500 cursor-pointer mt-20
            items-center mx-auto  
